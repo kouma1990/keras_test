25eaf0c43baa0aa3279eedb6920279de17bea70e
kouma1990 <kouma1990@gmail.com>
2017-06-29 08:03:49 +0900

    add: early_stopping

--- moon.py
+++ moon.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 #from keras.optimizers import SGD
 from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
@@ -25,7 +26,8 @@ model.add(Activation("sigmoid"))
 
 model.compile(loss="binary_crossentropy", optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999), metrics=["accuracy"])
 
-hist = model.fit(x_train, y_train, epochs=500, batch_size=20, validation_data=(x_train, y_train))
+early_stopping = EarlyStopping(monitor="val_loss", patience=10, verbose=1)
+hist = model.fit(x_train, y_train, epochs=500, batch_size=20, validation_data=(x_train, y_train), callbacks=[early_stopping])
 
 loss_and_metrics = model.evaluate(x_test, y_test)
 
